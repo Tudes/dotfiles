261e04d9200de68887ce3e2a6da1d1e9c5691812
Tudes <me@tudorcojocaru.ro>
2015-05-24 23:30:31 +0200

    brew-cask more to be added

--- brew-cask.sh
+++ brew-cask.sh
@@ -12,40 +12,40 @@ brew tap caskroom/versions
 
 # daily
 brew cask install alfred
-brew cask install divvy
+brew cask install bettertouchtool
 brew cask install dropbox
 brew cask install gyazo
-brew cask install onepassword
+brew cask install lastpass
 brew cask install rescuetime
+brew cask install slack
 # cask install flux
 
 # dev
 brew cask install iterm2
 brew cask install sublime-text
-brew cask install imagealpha
-brew cask install imageoptim
+# brew cask install imagealpha
+# brew cask install imageoptim
+brew cask install transmit
+brew cask install gitter
 
 # fun
-brew cask install limechat
-brew cask install miro-video-converter
-brew cask install horndis               # usb tethering
+# brew cask install limechat
+# brew cask install miro-video-converter
+# brew cask install horndis               # usb tethering
 
 # browsers
 brew cask install google-chrome-canary
-brew cask install firefox-nightly --force
-brew cask install webkit-nightly --force
+# brew cask install firefox-nightly --force
+# brew cask install webkit-nightly --force
 brew cask install chromium --force
-brew cask install torbrowser
+# brew cask install torbrowser
 
 # less often
-brew cask install disk-inventory-x
-brew cask install screenflow4 # 4 specifically not 5.
+# brew cask install disk-inventory-x
+# brew cask install screenflow4 # 4 specifically not 5.
+# brew cask install keycastr
+
+# personal
+brew cask install transmission
+brew cask install plex-home-theater
 brew cask install vlc
-brew cask install keycastr  
-
-
-# Not on cask but I want regardless.
-
-# 3Hub
-# File Multi Tool 5
-# Phosphor
